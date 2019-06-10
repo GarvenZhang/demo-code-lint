d33a439bbb5458ab697a950534e8efb47a1adb54
931923959 <931923959@qq.com>
2019-06-10 11:33:58 +0800

    test:

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 let a = 2;
-let b=3;
-let c=4
-function ii(){return a;}
-const r=4
-let oo =3
-let c=33
+let b = 3;
+let c = 4;
+function ii() {
+  return a;
+}
+const r = 4;
+let oo = 3;
+let c = 33;
